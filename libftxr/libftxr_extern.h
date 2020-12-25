34e26c3f58e736856af8d77792aa7b57786d4068
Joachim Metz <joachim.metz@gmail.com>
2020-12-25 07:00:40 +0100

    Applied updates

--- libftxr/libftxr_extern.h
+++ libftxr/libftxr_extern.h
@@ -30,7 +30,7 @@
 
 #include <libftxr/extern.h>
 
-#if defined( __CYGWIN__ )
+#if defined( __CYGWIN__ ) || defined( __MINGW32__ )
 #define LIBFTXR_EXTERN_VARIABLE	extern
 #else
 #define LIBFTXR_EXTERN_VARIABLE	LIBFTXR_EXTERN
